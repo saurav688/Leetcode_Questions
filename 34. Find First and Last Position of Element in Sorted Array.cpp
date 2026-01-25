ecf28a8567cb48b9fc73ed0bf544d42fdaa71464
Sourav_Tiwari <sourav688tiwari@gmail.com>
2026-01-25 21:51:59 +0530

    34. Find First and Last Position of Element in Sorted Array.cpp
    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
    
    If target is not found in the array, return [-1, -1].
    
    You must write an algorithm with O(log n) runtime complexity.
    
     
    
    Example 1:
    
    Input: nums = [5,7,7,8,8,10], target = 8
    Output: [3,4]
    Example 2:
    
    Input: nums = [5,7,7,8,8,10], target = 6
    Output: [-1,-1]
    Example 3:
    
    Input: nums = [], target = 0
    Output: [-1,-1]
     
    
    Constraints:
    
    0 <= nums.length <= 105
    -109 <= nums[i] <= 109
    nums is a non-decreasing array.
    -109 <= target <= 109

---
+++ 34. Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int first=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int last=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
+        if(first==n || nums[first]!=target)
+        return{-1,-1};
+        else
+        return{first,last};
+    }
+};
